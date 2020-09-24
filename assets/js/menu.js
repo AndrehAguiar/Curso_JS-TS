8640618a9264ded378f986d624f5c3a02b947bed
André Aguiar <andreaguiar.g@gmail.com>
2020-09-23 21:52:29 -0300

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const createMenu = function () {
         const navs = {
             'home': '/',
-            'imc': 'Curso_JS-TS/imcCalculator/',
-            'timer': './timer/',
-            'tasks': './lstTarefa/'
+            'imc': 'imcCalculator/',
+            'timer': 'timer/',
+            'tasks': 'lstTarefa/'
         };
 
         const menu = document.querySelector('#menu');
